218ed9155d11f7afe078f03302ad02d2abd65bd0
Oleg G.Kapranov <lugatex@yahoo.com>
2015-08-11 18:10:17 +0300

    edited validation

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,8 +2,8 @@ class Place < ActiveRecord::Base
   has_many :routes
 
   validates :name, presence: true
-  # validates :lat, presence: true, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
-  # validates :lon, presence: true, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
+  validates :lat, presence: true, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
+  validates :lon, presence: true, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
   validates :lat, presence: true
   validates :lon, presence: true
 end
